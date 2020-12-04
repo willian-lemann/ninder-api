7d2b65a2d97459849698029a1f1f2015b124bdf9
willian-lemann <willianleman@hotmail.com>
2020-12-04 00:40:10 -0300

    init

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -8,12 +27,16 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const http_1 = __importDefault(require("http"));
 const AppConfig_1 = __importDefault(require("./config/AppConfig"));
 const path_1 = __importDefault(require("path"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config({
+    path: process.env.NODE_ENV === 'dev' ? 'src/.env.local' : 'src/.env'
+});
 const PORT = process.env.PORT || 3333;
 const WebSocket_1 = require("./config/WebSocket");
 const app = express_1.default();
 const server = new http_1.default.Server(app);
 WebSocket_1.WebSocketConfig(server);
-mongoose_1.default.connect("mongodb+srv://willianlemann:American1995@ninder.ag4q2.mongodb.net/ninder-dev?retryWrites=true&w=majority", {
+mongoose_1.default.connect(`${process.env.CONNECTION_STRING}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
